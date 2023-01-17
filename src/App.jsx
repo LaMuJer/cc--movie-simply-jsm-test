75e7ce64d1eb2d0cb9dbe33258be052567dc7bd5
laMuJer <lareina1467.com>
2023-01-18 00:11:09 +0630

    movie

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,18 @@
+
+
+// 96aef73d
+
 import { useEffect } from "react"
-import { useState } from "react"
-
-
-const Person = () => {
-  return (
-    <>
-      <h1>Name : John </h1>
-      <h1>Age : 20 </h1>
-      <h1>Gender : Male </h1>
-
-    </>
-  )
-  
-}
-
 
 const App = () => {
-
-  const isShow = true
-
-  const [count, setCount] = useState(0)
+  const API_URL = 'http://www.omdbapi.com/?apikey=96aef73d'
 
   useEffect(() => {
-    setCount(100)
+    
   }, [])
 
-  const handlePlus = () => {
-    return setCount((pre) => pre + 1)
-  }
-
-  const handleMinus = () => {
-    return count > 0 ? setCount((pre) => pre - 1) : setCount(0)    
-  }
-
   return (
-    <div>
-
-      <Person/>
-
-      <button onClick={handlePlus}>+</button>
-      <h1>{count}</h1>
-      <button onClick={handleMinus}>-</button>
-
-
-    </div>
+    <div>App</div>
   )
 }
 
